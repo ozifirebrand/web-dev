4bbc65e482e1b306729802fd1b1949880edc77ea
Ozioma <oziomaokoroafor@gmail.com>
2021-11-04 16:33:19 +0100

    DOM manipulation third commit

--- ewaHtml/index.js
+++ ewaHtml/index.js
@@ -1,1 +1,12 @@
-console.log(document)
+// console.log(document.getElementsByTagName("Body")[0].innerHTML)
+document.addEventListener("DOMContentLoaded", ()=>{
+    let button = document.getElementsByClassName("register_btn")[0]
+    // console.log(button)
+    button.addEventListener("click", ()=> console.log("register_btn_clicked"))
+
+    function handleInputChange(e){
+        console.log(e.target.name, e.target.value)
+    }
+    let inputFields = document.getElementsByTagName("input")
+    inputFields.forEach(inputField => inputField.addEventListener("change", handleInputChange))
+})
