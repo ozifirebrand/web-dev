f98e167b7d3b3eabd6dab7602bc04eeea8129432
Ozioma <oziomaokoroafor@gmail.com>
2021-11-05 10:35:51 +0100

    Adding more methods

--- ewaHtml/index.js
+++ ewaHtml/index.js
@@ -1,16 +1,18 @@
-// console.log(document.getElementsByTagName("Body")[0].innerHTML)
+console.log(document.getElementsByTagName("Body")[0].innerHTML)
 document.addEventListener("DOMContentLoaded", ()=>{
+
+    let registerObject = {}
+
+    //buttonElement
     let button = document.getElementsByClassName("register_btn")[0]
-    // console.log(button)
     button.addEventListener("click", ()=> console.log("register_btn_clicked"))
 
+    //Input flow
     function handleInputChange(e){
+        registerObject = {... registerObject, [e.target.name]: e.target.value}
         console.log(e)
     }
 
     let inputFields = document.getElementsByTagName("input")
     Array.from(inputFields).forEach((inputField)=>inputField.addEventListener("input",(e)=> handleInputChange(e)))
-    // for (let i =0; i<inputFields.length; i++){
-    //     inputFields[i].addEventListener("input", (e)=>handleInputChange(e))
-    // }
 })
