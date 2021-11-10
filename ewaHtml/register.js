6136b71b5b5189ba47537d84405534bacdbf592f
Ozioma <oziomaokoroafor@gmail.com>
2021-11-10 16:00:41 +0100

    Register commit 2

--- ewaHtml/register.js
+++ ewaHtml/register.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", ()=>{
 
     let registerObject = {};
+    let loginObject = {};
     let repository = [
         {
             first_name: "ujay",
@@ -19,14 +20,14 @@ document.addEventListener("DOMContentLoaded", ()=>{
     function confirmRegistration() {
         if (Object.keys(registerObject).length === 4) {
             try {
-                let userRepository = localStorage.getItem("ewauser")
+                let userRepository = localStorage.getItem("ewausers")
                 if (userRepository === null) {
                     let repository = [registerObject]
-                    localStorage.setItem("ewauser", JSON.stringify(repository))
+                    localStorage.setItem("ewausers", JSON.stringify(repository))
                 } else {
                     let previousRepository = JSON.parse(userRepository)
                     let updatedRepository = [...previousRepository, registerObject]
-                    localStorage.setItem("ewauser", JSON.stringify(updatedRepository))
+                    localStorage.setItem("ewausers", JSON.stringify(updatedRepository))
                 }
             } catch (error) {
                 console.log(error)
@@ -45,17 +46,74 @@ document.addEventListener("DOMContentLoaded", ()=>{
             }
         }
     }
-    //buttonElement
-    let button = document.getElementsByClassName("register_btn")[0];
-    button.addEventListener("click", ()=> confirmRegistration());
 
-    //Input flow
+    //buttonElement
+    if (document.getElementsByClassName("register_btn").length>0){
+        let button = document.getElementsByClassName("register_btn")[0]
+        button.addEventListener("click", ()=>confirmRegistration())
+    }
+
+    function handleLoginChange(e){
+        loginObject = {... loginObject, [e.target.name]:e.target.value}
+    }
+
     function handleInputChange(e){
-        registerObject = {
-            ... registerObject, [e.target.name]: e.target.value
+        registerObject = { ... registerObject, [e.target.name]: e.target.value }
+    }
+
+    if (document.getElementsByClassName("register_btn").length>0){
+        let registerInputFields = document.getElementsByClassName("register_btn");
+        Array.from(registerInputFields).forEach((inputField)=>
+            inputField.addEventListener("input", (e)=>handleInputChange(e)))
+    }
+
+    if (document.getElementsByClassName("login_input").length > 0){
+        let loginInputFields = document.getElementsByClassName("login_input")
+        Array.from(loginInputFields).forEach((inputField)=>
+            inputField.addEventListener("input",(e)=>handleLoginChange(e)))
+    }
+
+    function confirmLogin(){
+        let userRepository =JSON.parse(localStorage.getItem("ewausers"))
+        let {email, password} = loginObject
+        let userObject = userRepository.find(user => user.email ===email)
+        if (userRepository.findIndex(user=>user.email ===email) !== -1){
+            if (userObject.password === password){
+                localStorage.setItem("ewauser", JSON.stringify(userObject))
+                window.location.href="home.html"
+            }else {
+                alert("Please enter a correct password")
+            }
+        }else {
+            alert("User does not exist")
         }
     }
 
+    if (document.getElementsByClassName("login_input").length>0){
+        let loginButton = document.getElementsByClassName("login_btn")[0]
+        loginButton.addEventListener("click", ()=> confirmLogin())
+    }
+
+    let registerInputFields = document.getElementsByClassName("register_input")
+    Array.from(registerInputFields).forEach((inputField)=>
+    inputField.addEventListener("input", (e)=>handleLoginChange(e)))
+    //Input flow
+
+
+    function confirmLogin(){
+        let userRepository = JSON.parse(localStorage.getItem("ewausers"))
+        let {email, password } = loginObject
+        if (userRepository.findIndex(user => user.email === email) !== -1){
+            if (userRepository.find(user => user.email === email).password === password) {
+                localStorage.setItem("ewauser", JSON.stringify(registerObject))
+                window.location.href="home.html"
+            }else {
+                alert("Please enter a correct password")
+            }
+        }else {
+            alert("User does not exist")
+        }
+    }
     let inputFields = document.getElementsByTagName("input");
     Array.from(inputFields).forEach((inputField)=>
         inputField.addEventListener("input",(e)=> handleInputChange(e)))
